a7473671b329d07cb6cc2dc8f88328ccbfa1d0cb
Gilang Osama <57081499+gilangosama@users.noreply.github.com>
2022-12-07 15:22:13 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FFD900',
+        secondary: '#1746F9',
+        three: '#1E2748',
+        four: '#111629',
+      },
+      fontFamily: {
+        raleway: ['Montserrat', 'sans-serif'],
+        nunito: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+};
