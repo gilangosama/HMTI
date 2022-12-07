15e80d076ae303a892ddf129473af0c98af89fcd
Gilang Osama <57081499+gilangosama@users.noreply.github.com>
2022-12-07 22:41:54 +0700

    nambah carousel

--- app.js
+++ app.js
@@ -18,3 +18,25 @@ document.querySelectorAll("li.has-child").forEach(function(anchor) {
         anchor.classList.toggle('active');
     });
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('slide');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+}
